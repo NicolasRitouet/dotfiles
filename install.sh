f88a46edd2d9f8bd889f66d1a2bc8241f89f5263
Nicolas Ritouet <nico2706@gmail.com>
2012-12-25 01:41:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -127,6 +127,7 @@ copySublimeDotFiles() {
 
 installSublimeText() {
 
+	sudo apt-get install -y python-software-properties
 	sudo add-apt-repository ppa:webupd8team/sublime-text-2
 	sudo apt-get update
 	sudo apt-get install sublime-text
