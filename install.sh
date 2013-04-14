d0cdbac794adb2bf385ffe9c3b53a267884c7364
NicolasRTT <nicolas@ritouet.com>
2013-04-14 15:27:18 +0200

    Refactoring, new choice between Server and Dev workstation

--- install.sh
+++ install.sh
@@ -6,10 +6,7 @@
 
 
 # Extra logs methods
-function desc_print() { echo -e "      ➜ $1 [0;35m$2[0m $3"; }
 function ask() { echo -e -n " [1;32m☆ $1 [0m\n"; }
-function happy_print() { echo -e "   [1;32m✔ $1[0;32m $2[0m"; }
-function sad_print() { echo -e "   [1;31m✖ $1[0;31m $2[0m"; }
 
 function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
 function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
@@ -18,16 +15,109 @@ function e_arrow()    { echo -e " \033[1;33m➜\033[0m  $@"; }
 
 e_header "Setup your Fresh Linux Box\n"
 
+# Check if root privileges
 if [ "$(whoami)" != "root" ]; then
 	e_arrow "Calling this script with user '$(whoami)'"
-	e_arrow "This script is limited since it wasn't called with sudo."
+	e_arrow "This script is limited since it wasn't called with root-level privileges."
 	e_arrow "To get the full functionnality, please type the following:"
 	e_arrow "sudo ./${0##*/}"
 	echo -e "\n"
 fi
 
+# Menu if root privileges
+menuRoot() {
+	PS3="Is this a [s]erver or a [d]eveloper workstation ?"
+	otpions=("Developer Box", "Server Box")
+	select opt in "${options[@]}"  "Quit"; do
+	    case "$REPLY" in
+	        d) # Developer Box
+				e_arrow "Developer Workstation"
+				menuDev
+				;;
+			s) # Server box
+				e_arrow "Server"
+				menuServer
+				;;
+			*)
+				echo invalid option
+				;;
+		esac
+	done
+}
 
-menu() {
+# sub-menu for a dev workstation
+menuDev() {
+	PS3="Developer Box: please enter your choice:"
+	options=("Copy dotfiles (.bashrc, .bash_aliases, .gitconfig, .vimrc, .tmux.conf, sublime-text)" #1
+		"Install Developer tools (git, svn, hg, maven, yeoman, nodejs, java7, etc..." #2
+	)
+	select opt in "${options[@]}"  "Quit"; do
+	    case "$REPLY" in
+	        1) # Copy dotfiles
+				copyDotfile .bashrc
+				copyDotfile .zork.theme.bash
+				copyDotfile .bash_aliases
+				copyDotfile .vimrc
+				copyDotfile .tmux.conf
+				copyDotfile .gitconfig
+				;;
+			2) # Install dev tools
+				installDevTools
+				;;
+	        $(( ${#options[@]}+1 )) )
+				echo "If you made some bash changes, don't forget to reload after leaving:"
+				echo ". ~/.bashrc";
+				exit 0
+	            break
+	            ;;
+	        *) echo invalid option;;
+	    esac
+	done
+}
+
+
+menuServer() {
+	PS3="Server Box: please enter your choice:"
+	options=("Update && upgrade your debian" #1
+		"Create a new user" #2
+		"Disable root login from SSH" #3
+		"Copy dotfiles (.bashrc, .bash_aliases, .gitconfig, .vimrc, .tmux.conf)" #4
+		"Install extras" #5
+	)
+	select opt in "${options[@]}"  "Quit"; do
+	    case "$REPLY" in
+	        1) # Update && upgrade debian
+				updateAndUpgrade
+	            ;;
+	        2) # Create a user
+	            configure_user
+	            ;;
+	       3) # Disable root
+	            configure_sshroot
+	            ;;
+	        4) # Copy dotfiles
+				copyDotfile .bashrc
+				copyDotfile .zork.theme.bash
+				copyDotfile .bash_aliases
+				copyDotfile .vimrc
+				copyDotfile .tmux.conf
+				copyDotfile .gitconfig
+				;;
+			5) # Install extras
+				install_extra
+				;;
+	        $(( ${#options[@]}+1 )) )
+				echo "If you made some bash changes, don't forget to reload after leaving:"
+				echo ". ~/.bashrc";
+				exit 0
+	            break
+	            ;;
+	        *) echo invalid option;;
+	    esac
+	done
+}
+
+menuNotRoot() {
 	PS3="Please enter your choice:"
 	options=("Copy dotfiles (.bashrc, .bash_aliases, .gitconfig, .vimrc, .tmux.conf)" #1
 	)
@@ -35,6 +125,7 @@ menu() {
 	    case "$REPLY" in
 	        1) # Copy dotfiles
 				copyDotfile .bashrc
+				copyDotfile .zork.theme.bash
 				copyDotfile .bash_profile
 				copyDotfile .bash_aliases
 				copyDotfile .vimrc
@@ -52,127 +143,61 @@ menu() {
 	done
 }
 
-menuRoot() {
-	PS3="Please enter your choice:"
-	options=("Update && upgrade your debian" #1
-		"Create a new user" #2
-		"Disable root login from SSH" #3
-		"Install bash-it and custom alias" #4
-		"Copy dotfiles (.bashrc, .bash_aliases, .gitconfig, .vimrc, .tmux.conf)" #5
-		"Install Git, clone dotfiles and symlink" #6
-		"Install Yeoman environment (node.js, ruby, etc...)" #7
-		"Install Sublime-text2 and copy Sublime-settings" #8
-		"Install extras" #9
-		"Create SSH Key for Github" #10
-	)
-	select opt in "${options[@]}"  "Quit"; do
-	    case "$REPLY" in
-	        1) # Update && upgrade debian
-				updateAndUpgrade
-	            ;;
-	        2) # Create a user
-	            configure_user
-	            ;;
-	       3) # Disable root
-	            configure_sshroot
-	            ;;
-	        4) # Install bash-it
-				installBashit
-				;;
-	        5) # Copy dotfiles
-				copyDotfile .bashrc
-				copyDotfile .bash_aliases
-				copyDotfile .vimrc
-				copyDotfile .tmux.conf
-				copyDotfile .gitconfig
-				;;
-			6) # Clone dotfiles
-				cloneDotfiles
-				;;
-			7) # Install yeoman
-				installYeoman
 
-				;;
-			8) # Install Sublime-text2 and copy settings
-				installSublimeText
-				copySublimeDotFiles
-				;;
-			9) # Install extras
-				install_extra
-				;;
-			10) # Create github SSH Key
-				ask "Work in progress, no working currently"
-				# createSSHKey
-				;;
-	        $(( ${#options[@]}+1 )) )
-				echo "If you made some bash changes, don't forget to reload after leaving:"
-				echo ". ~/.bashrc";
-				exit 0
-	            break
-	            ;;
-	        *) echo invalid option;;
-	    esac
-	done
+link_files () {
+  ln -s $1 $2
+  e_success "linked $1 to $2"
 }
 
-
-installBashit() {
-	e_arrow "Installing Bash-it"
-	# clone bash-it
-	if [ -d ~/.bash_it ]; then
-		e_arrow "Bash-it already cloned"
-	else
-		git clone http://github.com/revans/bash-it.git ~/.bash_it
-		if [ $? -gt 0 ]	# What did last command return ?
-		then
-			sad_print "Clone of bash-it" "fail!"
-		else
-			happy_print "Clone of bash-it" "Success"
-		fi
-	fi
-	# copy my own dotfiles to the custom dir of bash-it
-	mkdir -p ${HOME}/.bash_it/custom/aliases/
-	cp .bash_aliases ${HOME}/.bash_it/aliases/custom.aliases.bash
-	if [ $? -gt 0 ]	# What did last command return ?
-	then
-		sad_print "Copy of .bash_aliases to ${HOME}/.bash_it/aliases/custom.aliases.bash" "fail!"
-	else
-		happy_print "Copy of .bash_aliases to ${HOME}/.bash_it/aliases/custom.aliases.bash" "Success"
-	fi
-	# exec bash-it
-	~/.bash_it/install.sh
+copyDotBash() {
+	BASHFILES_ROOT="`pwd`/bash"
+	echo $BASHFILES_ROOT
+ 	for source in `find $BASHFILES_ROOT -maxdepth 2 -name \*`
+  	do
+  		echo $source
+  		# symlinkDotfile $source
+  	done
 }
 
+# Install dev Packages Defined In File extra
+installDevTools() {
+	e_arrow "Installing Dev tools"
+	# Loop Through Package List
+	while read package; do
+		# Install Currently Selected Package
+		installPackage $package
+	done < devTools
+	# Clean Cached Packages
+	apt-get clean
+	# Install node.js
+	add-apt-repository -y ppa:chris-lea/node.js
+	apt-get -q -y update
+	installPackage nodejs npm
 
-createSSHKey() {
-	# problem to solve: the public key seems to be saved into the wrong directory
-	SSH_DIR=/home/$SUDO_USER/.ssh
-	e_arrow "Creating a SSH Key into $SSH_DIR"
-	if [ -d $SSH_DIR ]; then
-		e_arrow "Backup of old Keys"
-		mkdir $SSH_DIR/key_backup
-		chown $SUDO_USER $SSH_DIR/key_backup
-		cp $SSH_DIR/id_rsa* $SSH_DIR/key_backup
-		rm $SSH_DIR/id_rsa*
-	else
-		mkdir $SSH_DIR
-		chown $SUDO_USER $SSH_DIR
-	fi
-	echo -n "Type your email, followed by [ENTER]:"
-	read EMAIL
-	ssh-keygen -t rsa -C "$EMAIL" -f $SSH_DIR/id_rsa
-	# Copy public key from root to $HOME?
+	# Install Ruby
+	e_arrow "Install Ruby..."
+	\curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby
+	e_success "Ruby Installed (or not)..."
+
+	# Install yeoman
+	e_arrow "Install Yeoman..."
+	npm install -g yo grunt-cli bower generator-angular generator-karma
+	e_success "Yeoman Installed (or not)..."
+
+
+	# Install java  oracle
+	add-apt-repository -y ppa:webupd8team/java
+	apt-get -q -y update
+	sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+	apt-get -y install oracle-java7-installer
+	echo -e "\n\nJAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/environment;
+	export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
+	java>/dev/null
 	if [ $? -gt 0 ]	# What did last command return ?
 	then
-		sad_print "Looks like ssh-keygen failed, try again"
+		e_error "Java install" "fail!"
 	else
-		installPackage xclip
-		xclip -sel clip < $SSH_DIR/id_rsa.pub
-		e_success "Your key was copied in your clipboard"
-		e_arrow "if not, please execute this:"
-		echo -e "xclip -sel clip < ${HOME}/.ssh/id_rsa.pub\n"
-		echo -e "Please go to your Github account Setting and add the key:"
-		echo -e "https://github.com/settings/ssh"
+		e_success "Java install" "Success"
 	fi
 }
 
@@ -224,15 +249,16 @@ installYeoman() {
 
 cloneDotfiles() {
 	if git --version &> /dev/null ; then
-		happy_print "GIT already installed"
+		e_success "GIT already installed"
 	else
 		installPackage git-core
 	fi
-	git clone https://github.com/NicolasRTT/dotfiles.git ~/dotfiles
-	cd ~/dotfiles
+	git clone https://github.com/NicolasRTT/dotfiles.git ~/.dotfiles
+	cd ~/.dotfiles
 	# add symlink for every dotfile
 
 	symlinkDotfile .bashrc
+	symlinkDotfile .zork.theme.bash
 	symlinkDotfile .bash_profile
 	symlinkDotfile .bash_aliases
 	symlinkDotfile .vimrc
@@ -243,10 +269,10 @@ cloneDotfiles() {
 updateAndUpgrade() {
 	e_arrow "Updating ..."
     	apt-get update > /dev/null
-	happy_print "apt-get update" "successful"
+	e_success "apt-get update" "successful"
 	e_arrow "Upgrading ..."
 	apt-get upgrade -y > /dev/null
-	happy_print "apt-get upgrade" "successful"
+	e_success "apt-get upgrade" "successful"
 }
 
 # Install Extra Packages Defined In File extra
@@ -282,9 +308,9 @@ configure_sshroot() {
 	sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 	if [ $? -gt 0 ]	# What did last command return ?
 	then
-		sad_print "Disable SSH Root login" "fail!"
+		e_error "Disable SSH Root login" "fail!"
 	else
-		happy_print "Disable SSH Root login" "Success"
+		e_success "Disable SSH Root login" "Success"
 		e_arrow "You need to restart sshd or reboot to take the changes"
 	fi
 }
@@ -294,14 +320,14 @@ copyDotfile() {
 	e_arrow "Copy $1 into ${HOME}"
 	if [ -f ${HOME}/$1 ]; then
 		cp ${HOME}/$1 ${HOME}/$1.backup
-		happy_print "$1 file backup as $1.backup"
+		e_success "$1 file backup as $1.backup"
 	fi
 	cp $1 ${HOME}/$1
 	if [ $? -gt 0 ]	# What did last command return ?
 	then
-		sad_print "Copy of $1" "FAIL"
+		e_error "Copy of $1" "FAIL"
 	else
-		happy_print "Copy of $1" "Successful!"
+		e_success "Copy of $1" "Successful!"
 	fi
 }
 
@@ -311,32 +337,10 @@ symlinkDotfile() {
 	ln -s $1 ${HOME}/$1
 	if [ $? -gt 0 ]	# What did last command return ?
 	then
-		sad_print "Symlink of $1" "FAIL"
+		e_error "Symlink of $1" "FAIL"
 	else
-		happy_print "Symlink of $1" "Successful!"
+		e_success "Symlink of $1" "Successful!"
 	fi
-}
-
-copySublimeDotFiles() {
-	sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 2/Packages
-	e_arrow "Copy Sublime settings"
-	e_arrow "Work in progress, try again later !"
-	#mv "$sublime_dir/User" "$sublime_dir/User.backup"
-	#cd "$sublime_dir"
-	#echo "Installing Soda Theme..."
-	#git clone https://github.com/buymeasoda/soda-theme/ "Theme - Soda"
-
-	#rm -rf $ZSH/sublime2/User
-	#cp -R ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User $ZSH/sublime2/User
-
-}
-
-installSublimeText() {
-
-	installPackage python-software-properties # Needed to call add-apt-repository
-	add-apt-repository ppa:webupd8team/sublime-text-2
-	apt-get update > /dev/null
-	installPackage sublime-text
 }
 
 # Install package, check if successfull and display fail or success
@@ -344,15 +348,15 @@ installPackage() {
 	while [ ${#} -gt 0 ]; do
 		INSTALLED=$(dpkg -l | grep "$1 ")
 		if [ "$INSTALLED" != "" ]; then
-			happy_print "Package $1 already installed!"
+			e_success "Package $1 already installed!"
 		else
 			e_arrow "Installing package $1"
 			apt-get install --force-yes --yes $1 > /dev/null 2>&1 ;
 			if [ $? -gt 0 ]	# What did last command return ?
 			then
-				sad_print "Install of $1" "FAIL"
+				e_error "Install of $1" "FAIL"
 			else
-				happy_print "Install of $1" "Successful!"
+				e_success "Install of $1" "Successful!"
 			fi
 		fi
 		shift
@@ -364,5 +368,5 @@ if [ "$(whoami)" = "root" ]
 then
 	menuRoot
 else
-	menu
+	menuNotRoot
 fi
